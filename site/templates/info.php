33ea7623db262ad7dabedd6497eb834ed2b82ae7
yuekaim <yuekaim@gmail.com>
2021-02-16 01:41:24 -0500

    profile image on info page

--- site/templates/info.php
+++ site/templates/info.php
@@ -13,7 +13,9 @@
 <?php if( $team = $page->team()->toStructure() ): ?>
 	<ul class="team">
 		<?php foreach( $team as $person ): ?>
+			<?php $image = $person->image()->toFile(); ?>
 			<li>
+				<img src="<?= $image->url() ?>" />
 				<h3><?= $person->name() ?></h3>
 				<p><?= $person->intro()->kirbytext() ?></p>
 			</li>
