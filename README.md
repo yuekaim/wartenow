818df5172d17e99e098726b44d9fdb425204133c
yuekaim <yuekaim@gmail.com>
2021-02-15 23:01:28 -0500

    small changes

--- README.md
+++ README.md
@@ -1,15 +1,3 @@
 ### Wartenow
-
-A small website for publishing recent activity happening at Wartenau Art Education, HFBK Hamburg.
-
-## Working on this project
-
-Get a copy of the code via Git:
-```bash
-git clone git@github.com:yuekaim/wartenow.git
-```
-
-Run a local PHP server, e.g. like:
-```bash
-php -S localhost:8000 kirby/router.php
-```
+HEELLOOOOOOOOOOOOO
+susususususaa
--- site/templates/default.php
+++ site/templates/default.php
@@ -3,32 +3,21 @@
 <article>
 
 	<header>
-
-		<h1><?= $page->title() ?></h1>
-
-		<?php if( $page->date()->isNotEmpty() ): ?>
-			<p class="date"><?= $page->date() ?></p>
-		<?php endif ?>
-
-		<?php if( $image = $page->titleImage()->toFile() ): ?>
-			<figure>
-				<img src="<?= $image->url() ?>" />
-			</figure>
-		<?php endif ?>
-
+		<h1><?= $page->pageTitle() ?></h1>
+		<p class="date"><?= $page->date() ?></p>
 	</header>
 
 	<main>
-		<?= $page->text()->kirbytext(); ?>
+		<?= $page->mainText()->kirbytext(); ?>
 	</main>
 
-	<?php if( $page->authors()->isNotEmpty() ): ?>
-		<ul class="authors">
-			<?php foreach( $page->authors()->split() as $author ): ?>
-				<li><?= $author ?></li>
-			<?php endforeach ?>
-		</ul>
-	<?php endif ?>
+	<ul class="authors">
+		<?php foreach( $page->author()->split() as $author ): ?>
+			<li><?= $author ?></li>
+		<?php endforeach ?>
+	</ul>
+
+	
 
 </article>
 
