5089bccbe575b85a6bda1dbd160db8b80d7ffe94
yuekaim <yuekaim@gmail.com>
2021-03-24 01:06:36 -0400

    print

--- assets/frontend/src/templates/post/index.svelte
+++ assets/frontend/src/templates/post/index.svelte
@@ -9,80 +9,101 @@
     import Image from '../../components/Image.svelte';
     import Links from '../../components/Links.svelte';
     import Debug from '../../components/Debug.svelte';
-    import Collapsable from '../../components/Collapsable.svelte';
     export let path;
 
 </script>
 
 <Load path="{path}" let:prop={page}>
+  <table>
 
-    <article>
+    <thead>
+      <tr>
+        <td>
+          <!--place holder for the fixed-position header-->
+          <div class="page-header-space"></div>
+        </td>
+      </tr>
+    </thead>
 
-        <header>
+    <tbody>
+      <tr>
+        <td>
+          <article>
 
-            {#if page.date}
-                <h3 class="date">{page.date}</h3>
-            {/if}
+              <header>
 
-            <h1 class="title">{page.title}</h1>
+                  {#if page.date}
+                      <h3 class="date">{page.date}</h3>
+                  {/if}
 
-            {#if page.subtitle}
-                <h2 class="subtitle">{page.subtitle}</h2>
-            {/if}
+                  <h1 class="title">{page.title}</h1>
 
-            {#if page.image}
-                <figure class="titleImg">
-                    <Image image={page.image} />
-                </figure>
-            {/if}
+                  {#if page.subtitle}
+                      <h2 class="subtitle">{page.subtitle}</h2>
+                  {/if}
 
-            {#if page.abstract}
-<<<<<<< HEAD
-                <div class="abstract">
-                  <h3>Abstract</h3>
-                  <p>{@html page.abstract}</p>
-                </div>
-=======
-                <Collapsable title="Abstract">
-                    <p class="abstract">{@html page.abstract}</p>
-                </Collapsable>
->>>>>>> 05df7c42ccfbfe5fb54b64d6be8e26cc7706f9be
-            {/if}
+                  {#if page.image}
+                      <figure class="titleImg">
+                          <Image image={page.image} />
+                      </figure>
+                  {/if}
 
-<<<<<<< HEAD
-            <p class="page-header print-only">author<br>
-<strong>{page.title}</strong> {page.subtitle}</p>
-=======
-            <p class="page-header">author<br>
-            <u>{page.title}</u> {page.subtitle}</p>
->>>>>>> fc92cba3ce2eedd8fa96f60e302798466772810f
+                  {#if page.abstract}
+                      <div class="abstract">
+                        <h3>Abstract</h3>
+                        <p>{@html page.abstract}</p>
+                      </div>
+                  {/if}
 
-            <div class="bg-circle"></div>
-        </header>
+                  <div class="bg-circle"></div>
+              </header>
 
-        <main class="text">
-            {@html page.content}
-        </main>
+              <main class="text">
+                  {@html page.content}
+              </main>
 
-        {#if page.footnotes}
-            <Footnotes text={page.footnotes} />
-        {/if}
+              {#if page.footnotes}
+                  <Footnotes text={page.footnotes} />
+              {/if}
 
-        <footer>
-            <Attributes attributes={page.attributes} />
+              <footer>
+                  <Attributes attributes={page.attributes} />
 
-            <Persons persons={page.authors}>
-                <h2>Autor*innen</h2>
-            </Persons>
+                  <Persons persons={page.authors}>
+                      <h2>Autor*innen</h2>
+                  </Persons>
 
-            <Links links={page.links} />
-            <Downloads downloads={page.downloads} />
-            <Tags tags={page.keywords} />
-        </footer>
+                  <Links links={page.links} />
+                  <Downloads downloads={page.downloads} />
+                  <Tags tags={page.keywords} />
+              </footer>
 
-        <!-- <Debug data={page} /> -->
+              <!-- <Debug data={page} /> -->
 
-    </article>
+          </article>
+        </td>
+      </tr>
+    </tbody>
+
+    <tfoot>
+      <tr>
+        <td>
+          <!--place holder for the fixed-position footer-->
+          <div class="page-footer-space"></div>
+        </td>
+      </tr>
+    </tfoot>
+
+  </table>
+
+    <!-- header and footer -->
+    <div class="page-header print-only">author<br>
+    <strong>{page.title}</strong> {page.subtitle}</div>
+
+    <div class="page-footer print-only">
+    I'm The Footer
+    </div>
+
 
 </Load>
 
@@ -103,5 +124,7 @@
             margin-bottom: 2rem;
         }
     }
+
+
 
 </style>
