6c1bb1156021cfcc93496411aaa2c24256741246
moritzebeling <git@moritzebeling.com>
2021-04-07 22:27:54 +0200

    blurred page transition

--- assets/frontend/src/templates/Load.svelte
+++ assets/frontend/src/templates/Load.svelte
@@ -1,22 +1,42 @@
 <script>
 
+    import { blur } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
 	import { load } from "../utilities/load.js";
 
     export let path;
+    let data;
+    let show, wait = false;
 
-    $: data = load( path );
+    function triggerFetch(){
+        show = false;
+        wait = true;
+        setTimeout(() => {
+            data = load( path, (d) => {
+                show = true;
+            });
+        }, 320);
+        setTimeout(() => {
+            wait = false;
+        }, 640);
+    }
+    $: loading = triggerFetch( path );
 
 </script>
 
-{#await data}
-    <p>Loading...</p>
-{:then page}
+{#if show && !wait}
+    <div transition:blur="{{amount: 10, duration: 300}}">
+        {#await data}
+            <p>Loading...</p>
+        {:then page}
 
-    <slot prop={page.data} />
+            <slot prop={page.data} />
 
-{:catch error}
-    <p class="error">Error</p>
-{/await}
+        {:catch error}
+            <p class="error">Error</p>
+        {/await}
+    </div>
+{/if}
 
 <style lang="scss">
 
--- assets/frontend/src/utilities/load.js
+++ assets/frontend/src/utilities/load.js
@@ -1,4 +1,4 @@
-export async function load( path ){
+export async function load( path, callback = false ){
 
     const url = `/json/${path}`;
     console.log( url );
@@ -8,6 +8,9 @@ export async function load( path ){
     const data = await res.json();
 
     if (res.ok) {
+        if( callback !== false ){
+            callback();
+        }
         return data;
     } else {
         throw new Error(data);
