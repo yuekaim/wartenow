d6d8adbfc148ede85d653be4da395f994cf39387
moritzebeling <git@moritzebeling.com>
2021-04-09 20:35:19 +0200

    fix footnotes

--- assets/frontend/src/templates/post/Footnotes.svelte
+++ assets/frontend/src/templates/post/Footnotes.svelte
@@ -13,21 +13,29 @@
 
 <style lang="scss">
 
+    .footnotes {
+        margin-top: 2rem;
+    }
     ol {
         list-style: none;
         counter-reset: my-awesome-counter;
         :global(li) {
             counter-increment: my-awesome-counter;
-            display: flex;
+            padding-left: 3em;
         }
         :global(li:before) {
-            display: block;
-            width: 3em;
+            display: inline-block;
+            width: 2.4em;
+            margin-left: -3em;
             content: counter(my-awesome-counter) ". ";
         }
         :global(.footnotereverse a) {
             display: inline-block;
             margin-left: 0.5em;
+        }
+        :global( li a ),
+        :global( li span ) {
+            display: inline;
         }
     }
 
