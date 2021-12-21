51ce2035027ccbc165ee750d2586c0a390a7b7fc
moritzebeling <git@moritzebeling.com>
2021-12-21 23:32:56 +0100

    setup languages

---
+++ site/languages/de.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+	'code' => 'de',
+	'default' => true,
+	'direction' => 'ltr',
+	'name' => 'Deutsch',
+	'url' => '/',
+	'slug' => [
+		'ä' => 'ae',
+		'ö' => 'oe',
+		'ü' => 'ue',
+		'ß' => 'ss',
+		'’' => '',
+		'\'' => '',
+	],
+	'locale'  => [
+		LC_ALL      => 'de_DE.utf8',
+		LC_COLLATE  => 'de_DE.utf8',
+		LC_MONETARY => 'de_DE.utf8',
+		LC_NUMERIC  => 'de_DE.utf8',
+		LC_TIME     => 'de_DE.utf8',
+		LC_MESSAGES => 'de_DE.utf8',
+		LC_CTYPE    => 'de_DE.utf8'
+	]
+];
---
+++ site/languages/en.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+	'code' => 'en',
+	'default' => false,
+	'direction' => 'ltr',
+	'name' => 'English',
+	'url' => 'en',
+	'slug' => [
+		'ä' => 'ae',
+		'ö' => 'oe',
+		'ü' => 'ue',
+		'ß' => 'ss',
+		'’' => '',
+		'\'' => '',
+	],
+	'locale'  => [
+		LC_ALL      => 'en_GB',
+		LC_COLLATE  => 'en_GB',
+		LC_MONETARY => 'en_GB',
+		LC_NUMERIC  => 'en_GB',
+		LC_TIME     => 'en_GB',
+		LC_MESSAGES => 'en_GB',
+		LC_CTYPE    => 'en_GB'
+	]
+];
